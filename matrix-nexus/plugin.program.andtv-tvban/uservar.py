5de172b898aabd53dc35fb29b9468c63f043df0e
andtv <132410413+andtvban@users.noreply.github.com>
2023-07-19 20:32:36 +0200

    31

--- matrix-nexus/plugin.program.andtv-tvban/uservar.py
+++ matrix-nexus/plugin.program.andtv-tvban/uservar.py
@@ -1,7 +1,8 @@
-import xbmcaddon
-
-import os
-
+#########################################################
+import xbmcaddon 
+                                      
+import os                                             
+#########################################################
 #########################################################
 #         Global Variables - DON'T EDIT!!!              #
 #########################################################
@@ -12,24 +13,24 @@ ART = os.path.join(PATH, 'resources', 'media')
 
 #########################################################
 #        User Edit Variables                            #
-#########################################################https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/builds.txt
-ADDONTITLE = '[COLOR azure][B]andtv[/B][/COLOR] [COLOR darkviolet][B]tvban[/B][/COLOR]'
+#########################################################
+ADDONTITLE = '[COLOR azure][B]Andtv[/B][/COLOR] [COLOR blueviolet][B]Nexus[/B][/COLOR]'
 BUILDERNAME = 'JoseAndTEBA & Andtv'
-EXCLUDES = [ADDON_ID, 'repository.andtv-tvban']
+EXCLUDES = [ADDON_ID, 'repository.Andtv-Nexus']
 # Text File with build info in it.
-BUILDFILE = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/builds.txt'
+BUILDFILE = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/buildfiles/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
 # Text File with apk info in it.  Leave as 'http://' to ignore
-APKFILE = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/apks.txt'
+APKFILE = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/buildfiles/apks.txt'
 # Text File with Youtube Videos urls.  Leave as 'http://' to ignore
 YOUTUBETITLE = '[B]Conciertos ANDTV[/B]'
-YOUTUBEFILE = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/youtube.txt'
+YOUTUBEFILE = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/buildfiles/youtube.txt'
 # Text File for addon installer.  Leave as 'http://' to ignore
 ADDONFILE = 'http://'
 # Text File for advanced settings.  Leave as 'http://' to ignore
-ADVANCEDFILE = 'https://raw.githubusercontent.com/andtvban/andtvban.github.io/master/andtv/wizard/xml/Advanced.json'
+ADVANCEDFILE = 'https://raw.githubusercontent.com/andtv/andtv.github.io/master/tvban/wizard/xml/Advanced.json'
 #########################################################
 
 #########################################################
@@ -62,7 +63,8 @@ SPACER = '<->'
 # You can edit these however you want, just make sure that you have a %s in each of the
 # THEME's so it grabs the text from the menu item
 #COLOR1 = 'limegreen'
-COLOR1 = 'mediumorchid'
+#COLOR1 = 'blueviolet'
+COLOR1 = 'orchid'
 COLOR2 = 'white'
 COLOR3 = 'dodgerblue'
 COLOR4 = 'turquoise'
@@ -95,7 +97,7 @@ CONTACTFANART = 'http://'
 # Enable Auto Update 'Yes' or 'No'
 AUTOUPDATE = 'Yes'
 # Url to wizard version
-WIZARDFILE = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/builds.txt' 
+buildfiles = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/buildfiles/builds.txt' 
 #########################################################
 
 #########################################################
@@ -104,11 +106,11 @@ WIZARDFILE = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/ma
 # Enable Auto Install 'Yes' or 'No'
 AUTOINSTALL = 'Yes'
 # Addon ID for the repository
-REPOID = 'repository.andtv-tvban'
+REPOID = 'repository.Andtv-Nexus'
 # Url to Addons.xml file in your repo folder(this is so we can get the latest version)
-REPOADDONXML = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/matrix-nexus/zips/addons.xml'
+REPOADDONXML = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/nexus/zips/addons.xml'
 # Url to folder zip is located in
-REPOZIPURL = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/matrix-nexus/zips/repository.andtv-tvban/'
+REPOZIPURL = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/nexus/zips/repository.Andtv-Nexus/'
 #########################################################
 
 #########################################################
@@ -117,14 +119,14 @@ REPOZIPURL = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/ma
 # Enable Notification screen Yes or No
 ENABLE = 'Yes'
 # Url to notification file
-NOTIFICATION = 'https://raw.githubusercontent.com/andtvban/repositorio.andtvban/master/buildmnfiles/andtvban/Notify.txt'
+NOTIFICATION = 'https://raw.githubusercontent.com/andtv/Repo-ANDTV/master/buildfiles/ANDTV/Notify.txt'
 # Use either 'Text' or 'Image'
 HEADERTYPE = 'Image'
 # Font size of header
 FONTHEADER = ''
-HEADERMESSAGE = '[COLOR azure][B]ANDTV[/B][/COLOR] [COLOR darkviolet][B]TVBAN[/B][/COLOR]'
+HEADERMESSAGE = '[COLOR azure][B]ANDTV[/B][/COLOR] [COLOR blueviolet][B]NEXUS[/B][/COLOR]'
 # url to image if using Image 424x180
-HEADERIMAGE = 'https://i.imgur.com/7wJW9zd.png'
+HEADERIMAGE = 'https://i.imgur.com/TipaCbZ.png'
 # Font for Notification Window
 FONTSETTINGS = ''
 # Background for Notification Window
